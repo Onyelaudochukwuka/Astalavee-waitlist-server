108766129d7e0ffca8c7e9e7754c3046333aba93
Udoka <udochukwukaonyela@gmail.com>
2022-10-25 23:50:46 +0100

    added types

---
+++ src/model/Waitlist.ts
@@ -0,0 +1,25 @@
+import { Schema, model } from "mongoose";
+// create waitlist schema
+const waitlistSchema = new Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+    },
+    affiliateMarketer: {
+        type: Boolean,
+        required: true,
+    },
+    contentCreator: {
+        type: Boolean,
+        required: true,
+    },
+    date: {
+        type: Date,
+        default: Date.now,
+    }
+});
+export const Waitlist = model('Group', waitlistSchema);
