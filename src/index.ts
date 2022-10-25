22c2df4d9f615780d56d5036577beb275e82ca4f
Udoka <udochukwukaonyela@gmail.com>
2022-10-25 23:50:23 +0100

    server side fixes

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import express, { Application } from "express";
+import { connect } from "mongoose";
 require("dotenv").config();
 const cors = require("cors");
 interface Server {
@@ -9,12 +10,21 @@ type Options = {
     limit: string;
     extended: boolean;
 };
-const options:Options = { limit: "10mb", extended: true };
+const options: Options = { limit: "10mb", extended: true };
 
 const app: Application = express();
 app.use(cors());
 app.use(express.json());
 // Define routes
 app.use("/user", require("./routes/message"));
+app.all("*", function (req, res, next) {
+    res.status(404).json({ errors: [{ message: "Route not found" }] });
+});
+const uri = process.env.MONGODB_URI;
+if (!uri) {
+    throw new Error("uri missing");
+}
 const PORT = 3000 || process.env.PORT;
-app.listen(PORT, () => console.log(`port started on Port ${PORT}`));
+connect(uri).then(() =>
+    app.listen(PORT, () => console.log(`port started on Port ${PORT}`))
+);
