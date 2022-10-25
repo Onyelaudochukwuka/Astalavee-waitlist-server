9727bf0673a745d39d0c1b36c66c51cbe7ca5a30
Udoka <udochukwukaonyela@gmail.com>
2022-10-25 23:53:54 +0100

    fixes

--- src/routes/waitlist.ts
+++ src/routes/waitlist.ts
@@ -3,20 +3,14 @@ require("dotenv").config();
 import { Request, Response, Router } from "express";
 import { body } from "express-validator";
 const router = Router();
-const { SendMail, sendMessageOnly } = require("../controllers/waitlist");
+const { joinWaitlist } = require("../controllers/waitlist");
 router.post(
     "/join-waitlist",
-    body("message", "A message from the user is required").isString(),
-    body("message", "Field Cannot be empty").notEmpty(),
-    body("user_email", "An email from the user is required").isEmail(),
-    body("user_email", "Field Cannot be empty").notEmpty(),
-    body("first_name", "The first Name of the user is required").isString(),
-    body("first_name", "Field Cannot be empty").notEmpty(),
-    body("last_name", "The last name is required").isString(),
-    body("last_name", "Field Cannot be empty").notEmpty(),
-    body("title", "Your message must have a title").isString(),
-    body("title", "Field Cannot be empty").notEmpty(),
-    body("my_email", "An email from user is required").isEmail(),
-    body("my_email", "Field Cannot be empty").notEmpty(),
-    SendMail,
+    body("name", "user's name is required").isString(),
+    body("email", "user's email is required").isEmail(),
+    body("affiliateMarketer", "affiliateMarketer is required").isBoolean(),
+    body("contentCreator", "contentCreator is required").isBoolean(),
+    joinWaitlist,
 );
+
+module.exports = router;
