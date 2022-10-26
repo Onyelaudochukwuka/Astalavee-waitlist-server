ff36487a99ba0ddc3b2475512d8fe10043a86382
Udoka <udochukwukaonyela@gmail.com>
2022-10-26 23:04:24 +0100

    added logic for controller

--- src/controllers/waitlist.ts
+++ src/controllers/waitlist.ts
@@ -3,7 +3,7 @@ require("dotenv").config();
 import type { Request, Response } from "express";
 import { ValidationError, validationResult } from "express-validator";
 import { Waitlist } from "../model/Waitlist";
-import {addMemberTopList} from "../lib/MainChimpInstance";
+import { addMemberTopList } from "../lib/MainChimpInstance";
 interface Res {
     message: string;
     success: boolean;
@@ -20,7 +20,7 @@ interface Req {
 }
 
 exports.joinWaitlist = async (req: Request<Req>, res: Response<Res>) => {
-    run();
+    
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
         return res
@@ -33,40 +33,27 @@ exports.joinWaitlist = async (req: Request<Req>, res: Response<Res>) => {
     }
     const { name, email, affiliateMarketer, contentCreator }: Req = req.body;
     const wait = await Waitlist.find({ name });
-    await Waitlist.deleteMany({});
     if (wait.length > 0) {
         return res.status(400).json({ success: false, message: "user already added to waitlist" });
     }
     else {
         if (affiliateMarketer || contentCreator) {
-            const courier = CourierClient({
-                authorizationToken: process.env.COURIER_AUTH_TOKEN,
+            addMemberTopList(email, name, "").then(async (subscriberId: string) => {
+                const waitlist = await Waitlist.create({
+                    name,
+                    email,
+                    affiliateMarketer,
+                    contentCreator,
+                    subscriberId
+                });
+                waitlist.save().then(() => {
+                    return res
+                        .status(200)
+                        .json({ success: true, message: "Message sent successfully" });
+                });
             });
-      const waitlist = await Waitlist.create({
-                name,
-                email,
-                affiliateMarketer,
-                contentCreator,
-            });
-            waitlist.save().then(async () => {
-                await courier.send({
-                    message: {
-                        to: {
-                            email
-                        },
-                        content: {
-                            title: "Waitlist confirmation",
-                            body: `Welcome to astalavee ${name}`,
-                        },
-                    },
-                }).then(() => {
-
-                });;
-                return res
-                    .status(200)
-                    .json({ success: true, message: "Message sent successfully" });
-            })
-        } else {
+        }
+        else {
             return res.status(400).json({ success: false, message: "Affiliate marketer or content creator should be true" });
         }
     }
